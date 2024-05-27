8b2a8bfe7c8416db52a2c20bb2dfef03d8c176f9
TyWalter <walterty89@gmail.com>
2024-05-27 13:38:33 -0500

    Added api/users functionality

---
+++ routes/api/users.js
@@ -0,0 +1,20 @@
+// Requiring in Express and controllers
+const router = require("express").Router();
+const {
+  getUsers,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+  createFriend,
+  deleteFriend
+} = require("../../controllers/userController");
+
+// /api/users
+router.route("/").get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route("/:userId").get(getUser).put(updateUser).delete(deleteUser)
+
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(createFriend).delete(deleteFriend)
